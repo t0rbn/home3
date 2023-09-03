584afbba270e0765b65949cb9b2f5db369ecfe55
t0rbn <torben@torben.xyz>
2023-09-03 11:48:08 +0200

    fix static paths root

--- server/src/controllers/StaticContentController.ts
+++ server/src/controllers/StaticContentController.ts
@@ -1,10 +1,19 @@
 import Controller from "./Controller";
 import express, {Application} from "express";
+import path from "path";
+import Logger from "../utils/Logger";
 
 export default class StaticContentController implements Controller {
+    private logger
+
+    constructor() {
+        this.logger = new Logger("StaticContentController")
+    }
+
     async registerEndpoints(app: Application): Promise<void> {
-        const clientPath = '../../client/build/'
-        app.use(express.static(clientPath));
+        const clientPath = path.join(__dirname, '../../../client/build')
+        this.logger.log("Serving static content from " + clientPath)
+        app.use(express.static(clientPath))
         app.get('*', (_req, res) => res.redirect('/'))
     }
 }
