302f9259e509e1491b8e119cb62f3ee88170ad3f
Torben Reetz <torben.reetz@mindmatters.de>
2024-11-01 19:36:01 +0100

    eh?

--- src/actions/tradfri-actions.ts
+++ src/actions/tradfri-actions.ts
@@ -5,11 +5,10 @@ import TrafriService from "@/services/TrafriService";
 
 export const getGroups: () => Promise<Array<TradfriGroup>> = async () => TrafriService.getGroups()
 
-
 export const getScenes: () => Promise<Array<TradfriScene>> = async () => TrafriService.getScenes()
-export const activateScene = (sceneId: number) => TrafriService.activateScene(sceneId)
+export const activateScene = async (sceneId: number) => TrafriService.activateScene(sceneId)
 
-export const setLightBrightness = (lightId: number, newBrightness: number)  => TrafriService.setLightBrightness(lightId, newBrightness)
-export const setLightColor = (lightId: number, newColor: string)  => TrafriService.setLightColor(lightId, newColor)
+export const setLightBrightness = async (lightId: number, newBrightness: number) => TrafriService.setLightBrightness(lightId, newBrightness)
+export const setLightColor = async (lightId: number, newColor: string) => TrafriService.setLightColor(lightId, newColor)
 
-export const  togglePlug = (plugId: number)  => TrafriService.togglePlug(plugId)
+export const togglePlug = async (plugId: number) => TrafriService.togglePlug(plugId)
