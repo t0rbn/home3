1261f2a29ec35f47d075b1fbe28e3131a3e6f6f7
Torben Reetz <torben.reetz@mindmatters.de>
2026-03-08 20:21:23 +0100

    add starting-style animations

--- src/components/layout/Layouts.tsx
+++ src/components/layout/Layouts.tsx
@@ -2,8 +2,13 @@ import {PropsWithChildren} from "react";
 import styles from "./layouts.module.css"
 import {cns} from "@/utils/cns";
 
-export function ListLayout(props: PropsWithChildren<{largeGap?: boolean}>) {
-    return <div className={cns(styles.listLayout, props.largeGap ? styles.large : undefined)}>{props.children}</div>
+export function ListLayout(props: PropsWithChildren<{largeGap?: boolean, animated?: boolean}>) {
+    const classNames = [
+        styles.listLayout,
+        props.largeGap ? styles.large : undefined,
+        props.animated ? styles.animated : undefined,
+    ]
+    return <div className={cns(...classNames)}>{props.children}</div>
 }
 
 export function HorizontalCenterLayout(props: PropsWithChildren) {
