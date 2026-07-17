680164f0fa6e5ab6b5a5b9378527fe34bf846924
Torben Reetz <torben.reetz@mindmatters.de>
2026-07-18 01:32:34 +0200

    remove dead files

--- src/utils/apiUrl.ts
+++
@@ -1,6 +0,0 @@
-import config from "../config.json"
-
-export function apiUrl(path:string) {
-    const foo = path.replaceAll(/\/+/gi, '/')
-    return `${config.api.base}${foo}`
-}
--- src/utils/colorUtils.ts
+++ src/utils/colorUtils.ts
@@ -1,7 +1,3 @@
-export function sanitizeColor(color: string) {
-    return `#${color.replace(/#/g, '')}`;
-}
-
 export function rgbArrayToHex(rgb: number[]) {
     return `#${rgb.map((v) => Math.floor(255 * v).toString(16).padStart(2, '0')).join('')}`;
 }
