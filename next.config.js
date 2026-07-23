530320c7e8f24caf069d0575712dbcccbb6954ee
Torben Reetz <torben.reetz@mindmatters.de>
2026-07-23 21:58:37 +0200

    use googlefonts cdn

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
     distDir: 'build',
     reactCompiler: true,
     cacheComponents: true,
-
-    async headers() {
-        return [
-            {
-                source: '/fonts/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,18 @@ export const metadata = {
 
 export default function RootLayout(props: PropsWithChildren<{ device: ReactNode }>) {
     return <html lang="en" className={cns(styles.root, globals.root)}>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=block"
+        />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=close,lightbulb_2,palette,power_settings_new,toggle_off,toggle_on&display=block"
+        />
+    </head>
     <body>
     {props.children}
     {props.device}
